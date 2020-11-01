2239968148fe8c06a25d30a5bab3e0630795acbc
Cole Strain <colestrain2000@gmail.com>
2020-11-01 11:49:54 -0800

    Added dummy home page using graceless-city as a template

---
+++ src/containers/home/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/common.css';
+import '../../styles/home.css';
+import TestCard from '../../images/TestCard.png'
+
+export default (props) => (
+  <div className="home">
+    <img src={TestCard} />
+  </div>
+);
