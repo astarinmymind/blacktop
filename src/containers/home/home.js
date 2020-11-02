79f6a3dcec68401d0b0d222b21e8418605570673
Cole Strain <colestrain2000@gmail.com>
2020-11-01 20:55:19 -0800

    The dummy home page is slightly less dumb now.

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import '../../styles/common.css';
 import '../../styles/home.css';
-import TestCard from '../../images/TestCard.png'
+import TestLogo from '../../images/TestLogo.png';
+import ChalkLine from '../../images/chalk-line.png';
 
 export default (props) => (
   <div className="home">
-    <img src={TestCard} />
+    <div className="manifest">
+      <img src={TestLogo}/>
+    </div>
+    <div className="line">
+      <img src={ChalkLine}></img>
+    </div>
+    <button>Host game</button>
+    <button>Join game</button>
   </div>
 );
