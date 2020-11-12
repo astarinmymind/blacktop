9392761bec11688ae18dfcd58784f5703300dc42
AngelaLu <angelfish@g.ucla.edu>
2020-11-12 12:34:18 -0800

    typescript create-react-app setup

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+class Home extends React.Component<{}, {}> {
+  public render() {
+    return (
+        <div>
+          <p>Hello</p>
+        </div>
+    );
+  }
+}
+
+export default Home;
