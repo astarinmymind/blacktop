fd5d84f532a9efe9119ba90e6880a382291dd82e
Cole Strain <colestrain2000@gmail.com>
2020-11-26 23:06:24 -0800

    Lobby codes are now enforced

--- client/src/components/home/home.tsx
+++ client/src/components/home/home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, DOMElement, useRef } from "react";
 import { useObserver } from 'mobx-react-lite'
 import { Link } from "react-router-dom";
 import './home.css';
@@ -14,7 +14,7 @@ import { usePlayerStore } from '../player/player'
 const gs = new GameService();
 
 
-export const Home = () => { 
+export const Home = () => {     
     const {playerStore} = usePlayerStore();
     
     const [lobbyId, setLobbyId] = React.useState("");
@@ -25,89 +25,97 @@ export const Home = () => {
 
         if (event.target.value === '' || re.test(event.target.value)) {
             setLobbyId(event.target.value);
+            gs.socket.emit("validId", event.target.value);
+            console.log(event.target.value);
         }
     }
+
+    const [isVaildId, setIsValidId] = React.useState(false);
+    useEffect(() => {
+        gs.socket.on("valID", function(data) {
+            setIsValidId(data);
+        })
+    }, []);
 
     function sendLobbyId() {
 		gs.socket.emit("joinLobby", lobbyId);
 		playerStore.lobbyId = parseInt(lobbyId);
     }
     
-    useEffect(() => {
-        gs.socket.on("lobbyWrong", function(data) {
-            alert("Please enter a valid six-digit lobby code!")
-        })
-    }, []);
+    const ConditionalLink = () => isVaildId
+      ? <Link to="/Lobby"><button onClick={ sendLobbyId }>Join game</button></Link>
+      : <><button onClick={ sendLobbyId }>Join game</button></>;
+    console.log(isVaildId);
 
     return useObserver(() => (
-            <div style={{backgroundColor: "rgb(14, 14, 14)"}}>
-                <div className="manifest">
-                    <img src={ TestLogo }/>
-                </div>
-                <img src={ChalkLine} />
-                <Link to="/lobby" style={{ display: "flex", justifyContent: "center" }}>
+        <div style={{backgroundColor: "rgb(14, 14, 14)"}}>
+            <div className="manifest">
+                <img src={ TestLogo }/>
+            </div>
+            <img src={ChalkLine} />
+            <div style={{ display: "flex", justifyContent: "center" }}>
+                <Link to="/lobby">
                     <button onClick={ makeLobby }>Host game</button>
-                    <button onClick={ sendLobbyId }>Join game</button>
                 </Link>
+                <input 
+                    name="lobbyId" 
+                    type="text" 
+                    maxLength={6} 
+                    placeholder="Lobby code" 
+                    value={ lobbyId }
+                    onChange={ updateLobbyId }
+                />
+                <ConditionalLink />
+            </div>
                 <br />
-                <div style={{ display: "flex", justifyContent: "center" }}>
-                    <input 
-                            name="lobbyId" 
-                            type="text" 
-                            maxLength={6} 
-                            placeholder="Lobby code" 
-                            value={ lobbyId }
-                            onChange={ updateLobbyId }
-                    />
-                </div>
                 <br />
-                <div className="columns">
-                    <div>
-                    </div>
-                    <div>
-                        <h1>Welcome to Blacktop!</h1>
-                        Blacktop is a game about deception, strategy, and sabotage! 
-                        <br />Each player brings with them a personalized side deck to assist them as they try to not get kazaap'd!
-                        <br />You can join a public game or play a private game with your friends!
-                        <br />
-                    </div>
-                    <div>
-                        <img src={PlushCat}/>
-                    </div>
+            <div className="home-columns">
+                <div>
                 </div>
-                <div className="columns">
-                    <div>
-                        <img src={Werewolf}/>
-                    </div>
-                    <div>
-                        <h1>Rules</h1>
-                        The goal of the game is to get your point total as close to 100 at the end of the final round without going over. 
-                        The game is split up into rounds, with each round consisting of 3 turns. 
-                        There are point cards, action cards, and bomb cards. 
-                        During a turn, each player has the option to play cards, and then they must draw. 
-                        At the end of each round, if a player’s point total is a multiple of ten, a special effect happens based on which ten they are on. 
-                        The final round starts the first time a player’s point total is over a hundred at the end of the round.
-                        <br />
-                        <h2>Play Cards</h2>
-                        <br />
-                        <h2>Special Effects</h2>
-                        <br />
-                    </div>
-                    <div>
-                    </div>
+                <div>
+                    <h1>Welcome to Blacktop!</h1>
+                    Blacktop is a game about deception, strategy, and sabotage! 
+                    <br />Each player brings with them a personalized side deck to assist them as they try to not get kazaap'd!
+                    <br />You can join a public game or play a private game with your friends!
+                    <br />
                 </div>
-                <div className="columns">
-                    <div>
-                    </div>
-                    <div>
-                        <h1>Cards</h1>
-                        <br />
-                    </div>
-                    <div>
-                        <img src={TestCard}/>
-                    </div>
+                <div>
+                    <img src={PlushCat}/>
                 </div>
             </div>
+            <div className="home-columns">
+                <div>
+                    <img src={Werewolf}/>
+                </div>
+                <div>
+                    <h1>Rules</h1>
+                    The goal of the game is to get your point total as close to 100 at the end of the final round without going over. 
+                    The game is split up into rounds, with each round consisting of 3 turns. 
+                    There are point cards, action cards, and bomb cards. 
+                    During a turn, each player has the option to play cards, and then they must draw. 
+                    At the end of each round, if a player’s point total is a multiple of ten, a special effect happens based on which ten they are on. 
+                    The final round starts the first time a player’s point total is over a hundred at the end of the round.
+                    <br />
+                    <h2>Play Cards</h2>
+                    <br />
+                    <h2>Special Effects</h2>
+                    <br />
+                </div>
+                <div>
+                </div>
+            </div>
+            <div className="home-columns">
+                <div>
+                </div>
+                <div>
+                    <h1>Cards</h1>
+                    <br />
+                </div>
+                <div>
+                    <img src={TestCard}/>
+                </div>
+            </div>
+        </div>
     ));
 	function makeLobby() 
 	{
