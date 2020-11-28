dde811f5e705645901c9dacfff30e9ec33929260
AngelaLu <angelfish@g.ucla.edu>
2020-11-28 15:53:13 -0800

    added player hands and base game setup

--- client/src/components/game/game.tsx
+++ client/src/components/game/game.tsx
@@ -6,13 +6,18 @@ Notes:
     - let you group a list of children without adding extra nodes to the DOM
 */
 
-import { CardStore } from "../card/CardStore";
+// import { CardStore } from "../card/CardStore";
   
 //export const Game = () => {
 import React, { useState, useEffect } from "react";
 import { usePlayerStore } from '../player/player'
 import { useObserver } from 'mobx-react-lite'
-import { useCardStore } from '../card/card'
+import card from '../card/card'
+// import { useCardStore } from '../card/card'
+
+import GameService from '../../services/GameService';
+
+const gs = new GameService();
 
 //Just a random ID i used to test that each client was different
 var id = Math.floor(Math.random() * 100);
@@ -23,18 +28,39 @@ function Game() {
 	//this is syntax I use to set variables, setTitle being the way to change them
 
   // gets store
-  const {cardStore} = useCardStore()
+  // const {cardStore} = useCardStore()
   const {playerStore} = usePlayerStore();
 
-  console.log(cardStore)
+  // console.log(cardStore)
+
+  // emits socket event that player has pressed start game
+  function startGame() 
+  {
+      gs.socket.emit("gameStarted", playerStore.lobbyId);
+  }
+
+  // emits socket event that player has pressed start game
+  function drawCard() 
+  {
+      gs.socket.emit("cardDrawn", playerStore.lobbyId);
+      console.log("card drawn");
+  }
+
+  // emits socket event that player has pressed start game
+  function playCard() 
+  {
+      gs.socket.emit("cardPlayed", playerStore.lobbyId);
+      console.log("card played");
+  }
   
   return useObserver(() => (
     // renders an unordered list of cards
     //button with the variable grabbed from the server
     <>
-      <button onClick={() => cardStore.addCard('card string', 0)}>Draw Card</button>
+      <button onClick={drawCard}>Draw Card</button>
+      <button onClick={playCard}>Play Card</button>
       <ul>
-        {cardStore.cards.map(card => (<li>{card}</li>))}
+        {playerStore.playerHand.map(card => (<li>{card}</li>))}
       </ul>
     </>
   ));
