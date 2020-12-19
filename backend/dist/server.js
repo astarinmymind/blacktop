cf575e63d2a176d5eff0ae0258fc3a5e0b9babea
AngelaLu <angelfish@g.ucla.edu>
2020-12-19 11:15:36 -0800

    deleted backend folder

--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.app = void 0;
-const ChatServer_1 = require("./ChatServer");
-let app = new ChatServer_1.ChatServer().app;
-exports.app = app;
-//# sourceMappingURL=server.js.map
