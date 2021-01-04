1efdc080c56a3ef32f3993543b5f51e264ad6961
Cole Strain <colestrain2000@gmail.com>
2021-01-03 20:35:16 -0800

    changed socket endpoint once again

--- client/src/services/GameService.ts
+++ client/src/services/GameService.ts
@@ -1,7 +1,7 @@
 import socketIOClient from "socket.io-client";
 
-const ENDPOINT = "http://127.0.0.1:5000";
-const sock = socketIOClient("https://salty-headland-36058.herokuapp.com:5000"); 
+const ENDPOINT = "http://172.17.18.238:5000";
+const sock = socketIOClient(ENDPOINT); 
 
 class GameService {
 	socket;
